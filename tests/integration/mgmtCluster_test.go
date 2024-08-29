bcbc917f0ddf0e4ab25b18b9d724f4ee3a2186a1
Eric Promislow <epromislow@suse.com>
2024-08-29 15:18:50 -0700

    Found another instance of 'rke/types' in a test.

--- tests/integration/mgmtCluster_test.go
+++ tests/integration/mgmtCluster_test.go
@@ -1,31 +1,36 @@
 package integration_test
 
 import (
+	"encoding/json"
+	"github.com/stretchr/testify/require"
+
 	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
-	"github.com/rancher/rke/types"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (m *IntegrationSuite) TestManagementCluster() {
 	newObj := func() *v3.Cluster { return &v3.Cluster{} }
-	validCreateObj := &v3.Cluster{
-		ObjectMeta: v1.ObjectMeta{
-			Name: "test-cluster",
-		},
-		Spec: v3.ClusterSpec{
-			ClusterSpecBase: v3.ClusterSpecBase{
-				RancherKubernetesEngineConfig: &types.RancherKubernetesEngineConfig{},
-			},
-		},
-	}
+
+	var validCreateObj v3.Cluster
+	// Use a json-encoded representation of the desired data
+	// to avoid loading the RKE types module.
+	s := `{
+  "metadata": {
+    "name": "test-cluster"
+  },
+  "spec": {
+    "rancherKubernetesEngineConfig": { }
+  }
+}`
+	err := json.Unmarshal([]byte(s), &validCreateObj)
+	require.NoError(m.T(), err, "Failed to create v3.Cluster object from json")
 
 	validDelete := func() *v3.Cluster {
-		return validCreateObj
+		return &validCreateObj
 	}
 	endPoints := &endPointObjs[*v3.Cluster]{
 		invalidCreate:  nil,
 		newObj:         newObj,
-		validCreateObj: validCreateObj,
+		validCreateObj: &validCreateObj,
 		invalidUpdate:  nil,
 		validUpdate:    nil,
 		validDelete:    validDelete,
